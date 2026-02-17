8ddf9673e801c818ea428228bbe00889c3138d89
Lily <info@wistaway.com>
2026-02-16 21:52:27 -0600

    refactor: finalize design system and global app shell

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Playlists from "./pages/Playlists";
 import PlaylistDetail from "./pages/PlaylistDetail";
 import "./styles/design-system.css";
 
+const bg = new URL("./assets/Background Image.png", import.meta.url).href;
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/playlists" element={<Playlists />} />
-      <Route path="/playlists/:id" element={<PlaylistDetail />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    <div className="app-shell" style={{ backgroundImage: `url(${bg})` }}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/playlists" element={<Playlists />} />
+        <Route path="/playlists/:id" element={<PlaylistDetail />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
   );
 }
